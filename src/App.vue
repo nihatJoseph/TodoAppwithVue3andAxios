cffde93712766e6f054365630133f2aaefe1b1d7
Nihat Yusuf <nihatyusufuslu@gmail.com>
2022-09-05 16:07:46 +0300

    Todo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h3>Alışveriş Listesi</h3>
+    <div class="my-2">
+      <input type="text" placeholder="ne alacaksın?" @keydown.enter="onSave" />
+    </div>
+    <ul v-if="itemsList.length > 0">
+      <li v-for="item in itemsList" :key="item.id" class="d-flex justify-content-between align-items-center">
+        <span>{{ item.title }}</span>
+        <button @click="onDelete(item)" class="sm red">Sil</button>
+      </li>
+    </ul>
+    <div v-else class="bg-blue text-white">Herhangi bir ürün yoltur...</div>
+    <small class="mt-2 text-red d-flex justify-content-end align-items center"> {{ itemCount }} adet alınacak ürün vardır.</small>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      itemsList: [],
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:3000/items").then((items_response) => {
+      this.itemsList = items_response.data || [];
+    });
+  },
+  methods: {
+    onSave(e) {
+      axios.post("http://localhost:3000/items", { title: e.target.value, created_at: new Date(), completed: false }).then((save_response) => {
+        console.log(save_response);
+        this.itemsList.push(save_response.data);
+        e.target.value = "";
+        e.target.focus();
+      });
+    },
+    onDelete(item) {
+      axios.delete(`http://localhost:3000/items/${item.id}`).then((delete_response) => {
+        console.log("delete_response :>> ", delete_response);
+        this.itemsList = this.itemsList.filter((i) => i.id != item.id);
+      });
+    },
+  },
+  computed: {
+    itemCount() {
+      return this.itemsList.length || 0;
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
